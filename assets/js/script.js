2631946217708b1a16cf957064288a9d3938a8da
leamr10 <leamr10@gmail.com>
2023-06-07 17:11:01 -0400

    changed userInput function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,29 @@ var options = [];
 
 function generatePassword () {
     var userInput =window.prompt ("Choose a password length between 8 and 128 characters ");
-    if (userInput < 8 && userInput > 128) { generatePassword() }
+    if (userInput < 8 || userInput > 128) { generatePassword() }
 
     var lowerCase =window.confirm ("Include lowercase characters? ");
     console.log(lowerCase);
-    if (lowerCase == true){
+    if (lowerCase){
         var lowerLetters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
         options.push(...lowerLetters);
     }
     var upperCase= window.confirm ("Include uppercase characters? ");
     console.log(upperCase);
-    if (upperCase == true) {
+    if (upperCase) {
         var upperLetters= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
         options.push(...upperLetters);
     }    
     var numeric= window.confirm ("Include numeric characters? ");
     console.log(numeric);
-    if (numeric== true) {
+    if (numeric) {
         var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
         options.push(...numeric);
     }
     var specChar= window.confirm ("Include special characters? ");
     console.log(specChar);
-    if (specChar==true){
+    if (specChar){
         var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "{", "]", "}", ";", ":", "'", '"', ",", "<", ".", ">", "?", "/"];
         options.push(...specChar);
     }
